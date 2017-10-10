4542fb1fc1e29b63c1136fbb7b6dd9be2c9601ac
SamuelDennis <mdennis@scienceleadership.org>
2017-10-10 09:49:37 -0400

    I added the Choice Box code, as well as adding 2 more images to choose from once the Choice Box is up and running

--- src/edu/sla/Main.java
+++ src/edu/sla/Main.java
@@ -12,7 +12,8 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
+import javafx.scene.control.ChoiceBox;
+
 
 public class Main extends Application {
 
@@ -22,7 +23,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Button clickMeButton = new Button("If you would like a Christmas Shiba, give me a gentle tap on the front");
+        Button clickMeButton = new Button("Begin the Onslaught of the Christmas Shibas! Show them no mercy!");
         clickMeButton.setOnAction(e -> christmasShibaButton_click());
         StackPane myUIFrame1 = new StackPane();
         primaryStage.setTitle("Shiba, Shiba, Oh Christmas Shiba");
@@ -31,9 +32,27 @@ public class Main extends Application {
         Image image = new Image("ChristmasShiba.jpg");
         ImageView imageView = new ImageView(image);
 
+        Image image2 = new Image("ChristmasShiba2.jpg");
+        ImageView imageView2 = new ImageView(image);
+
+        Image image3 = new Image("ChristmasShiba 3.jpg");
+        ImageView imageView3 = new ImageView(image);
+
         HBox ChristmasShibaHBox  = new HBox(imageView);
 
         Scene afterChristmasShiba = new Scene(ChristmasShibaHBox, 325, 400);
+
+        ChoiceBox choiceBox = new ChoiceBox();
+
+        choiceBox.getItems().add("The First Christmas Shiba!");
+        choiceBox.getItems().add("The One AFTER The First Christmas Shiba!");
+        choiceBox.getItems().add("The One and Only, FOURTH Christmas Shiba!");
+
+        HBox hbox = new HBox(choiceBox);
+
+        Scene scene = new Scene(hbox, 200, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
         StackPane myUIFrame2 = new StackPane();
         myUIFrame1.getChildren().addAll(clickMeButton, ChristmasShibaHBox);
