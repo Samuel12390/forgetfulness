a8d6ff256f7ce64941f968412a9285aa1656ec52
SamuelDennis <mdennis@scienceleadership.org>
2017-10-10 10:32:07 -0400

    I added the Choice Box code, as well as adding 2 more images to choose from once the Choice Box is up and running

--- src/edu/sla/Main.java
+++ src/edu/sla/Main.java
@@ -6,29 +6,41 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.beans.value.*;
 
 
 public class Main extends Application {
 
     public void christmasShibaButton_click() {
-        System.out.println("You are like everyone else, and would like a Christmas Shiba.");
     }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Shibas, Shibas! Oh, the Christmas Shibas");
+        ChoiceBox shibaChoiceBox = new ChoiceBox();
 
-        Button clickMeButton = new Button("Begin the Onslaught of the Christmas Shibas! Show them no mercy!");
+        shibaChoiceBox.getItems().add("The First Christmas Shiba!");
+        shibaChoiceBox.getItems().add("The One AFTER The First Christmas Shiba!");
+        shibaChoiceBox.getItems().add("The One and Only, FOURTH Christmas Shiba!");
+
+        shibaChoiceBox.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number number2) {
+                System.out.println(shibaChoiceBox.getItems().get((Integer) number2));
+            }
+        });
+
+
+
+        Button clickMeButton = new Button("Begin the onslaught of the Christmas Shibas! Show them no mercy!");
+
         clickMeButton.setOnAction(e -> christmasShibaButton_click());
-        StackPane myUIFrame1 = new StackPane();
-        primaryStage.setTitle("Shiba, Shiba, Oh Christmas Shiba");
 
-        Scene buttonBoi = new Scene(myUIFrame1, 325, 400);
         Image image = new Image("ChristmasShiba.jpg");
         ImageView imageView = new ImageView(image);
 
@@ -39,28 +51,14 @@ public class Main extends Application {
         ImageView imageView3 = new ImageView(image);
 
         HBox ChristmasShibaHBox  = new HBox(imageView);
+        HBox horizontalControls = new HBox(image, clickMeButton, shibaChoiceBox);
 
-        Scene afterChristmasShiba = new Scene(ChristmasShibaHBox, 325, 400);
 
-        ChoiceBox choiceBox = new ChoiceBox();
+        Scene afterChristmasShiba = new Scene(horizontalControls, 325, 400);
 
-        choiceBox.getItems().add("The First Christmas Shiba!");
-        choiceBox.getItems().add("The One AFTER The First Christmas Shiba!");
-        choiceBox.getItems().add("The One and Only, FOURTH Christmas Shiba!");
-
-        HBox hbox = new HBox(choiceBox);
-
-        Scene scene = new Scene(hbox, 200, 100);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-        StackPane myUIFrame2 = new StackPane();
-        myUIFrame1.getChildren().addAll(clickMeButton, ChristmasShibaHBox);
         primaryStage.setScene(afterChristmasShiba);
-        primaryStage.setScene(buttonBoi);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         Application.launch(args);
